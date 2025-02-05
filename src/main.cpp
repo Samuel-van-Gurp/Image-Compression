025b8277ada5cc0e361139929908fd12ae243af3
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-05 10:27:32 +0100

    try DFT iDFT

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
+#include "Image.h"
+#include "Compressor.h"
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, CMake with VS Code on Windows!" << std::endl;
+    
+    
+    
+    Image img = Image("C:/Users/svangurp/Desktop/projects/ImageCompression/images/GrayscaleTestImg/lena1.tif");
+
+    img.displayImage();
+
+    Compressor compressor = Compressor();
+
+    // get the DFT of the image
+    cv::Mat dftImage = compressor.DFT(img.getImage());
+
+    // get the IDFT of the image
+    Image reconImg = Image(compressor.IDFT(dftImage));
+
+    // display the IDFT image
+    reconImg.displayImage();
+    
     return 0;
 }
