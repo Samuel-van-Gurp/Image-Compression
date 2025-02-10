7da7b7d3d3602c0657f66f8603a4c00c27c57b13
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-10 16:59:03 +0100

    misc

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
 #include "Image.h"
 #include "Compressor.h"
 #include "SparseRepresentation.h"
+#include "StoringData.h"
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main() {
-    
-    Image img = Image("C:/Users/svangurp/Desktop/projects/ImageCompression/images/GrayscaleTestImg/lena1.tif");
+int main()
+{
+
+    Image img = Image("C:/Users/svangurp/Desktop/projects/ImageCompression/images/GrayscaleTestImg/camera.tif");
 
     Compressor compressor = Compressor();
-    
-    img.displayImage();
 
-    SparseRepresentations sparseRepr = compressor.compress(img, 1);
+    // img.displayImage();
 
-    Image decompressedImage = compressor.decompress(sparseRepr);
+    SparseRepresentation sparseRepr = compressor.compress(img, 10);
+
+    StoringData storingData = StoringData();
+
+    // std::cout<< "sparseRepr.getSize().first" << sparseRepr.getSize().first<<std::endl;
+    storingData.SaveFile("camera.samuel", "C:/Users/svangurp/Desktop/projects/ImageCompression/images/imgOUT/", sparseRepr);
+
+    auto sparseReprloaded = storingData.LoadFile("camera.samuel", "C:/Users/svangurp/Desktop/projects/ImageCompression/images/imgOUT/");
+
+    // std::cout<< "sparseReprloaded.getSize().first" << sparseReprloaded.getSize().first<<std::endl;
+
+    Image decompressedImage = compressor.decompress(sparseReprloaded);
 
     decompressedImage.displayImage();
 
