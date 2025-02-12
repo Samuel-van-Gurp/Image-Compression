8212bafa9f0b99f7f6dafd0d044f8baebcfd718e
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-12 10:56:04 +0100

    Add compression round-trip tests for Compressor class

--- tests/test_Compressor.cpp
+++ tests/test_Compressor.cpp
@@ -1,6 +1,26 @@
 #include "Compressor.h"
+#include "Image.h"
 #include <gtest/gtest.h>
 
-TEST(CompressorTest, HandelsInvalidFilesPath)
+TEST(CompressorTest, CompressionRountTripNOCompression)
 {
+    // Arrange
+    float COMPRESSION_PERCENTILE = 1.0;
+
+    cv::Mat RandomMatrix(256, 256, CV_8UC1);
+    cv::randu(RandomMatrix, 0.0, 255.0);
+
+    Image randomImage = Image(RandomMatrix);
+    Compressor compressor;
+
+    auto SparceRep = compressor.compress(randomImage, COMPRESSION_PERCENTILE);
+    Image decompressedImage = compressor.decompress(SparceRep);
+
+    // Assert
+
+    // cal avg per pixel err
+    double totalError = cv::norm(RandomMatrix, decompressedImage.getImageMatrix(), cv::NORM_L2);
+    double avgError = totalError / static_cast<double>(RandomMatrix.total());
+
+    EXPECT_NEAR(avgError, 0.0, 1); // acount for floating point err
 }
