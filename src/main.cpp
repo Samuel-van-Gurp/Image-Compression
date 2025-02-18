d2e3d5eb367d730431ba9dbc9d7ce961ff91d955
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-18 17:12:52 +0100

    misc

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 #include "SparseRepresentation.h"
 #include "StoringData.h"
 #include "DCT/DCT.h"
+#include "DCT/ImageChopper.h"
+#include "DCT/DCTCompression.h"
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
 int main()
 {
 
-        // Image img = Image("C:/Users/svangurp/Desktop/projects/ImageCompression/images/GrayscaleTestImg/camera.tif");
+    // Image img = Image("C:/Users/svangurp/Desktop/projects/ImageCompression/images/GrayscaleTestImg/camera.tif");
 
     // Compressor compressor = Compressor();
 
@@ -29,6 +31,26 @@ int main()
     // Image decompressedImage = compressor.decompress(sparseReprloaded);
 
     // decompressedImage.displayImage();
+
+    ////////////////////////
+    //  DCT compression   //
+    ////////////////////////
+
+    Image img = Image("C:/Users/svangurp/Desktop/projects/ImageCompression/images/GrayscaleTestImg/camera.tif");
+    img.displayImage();
+    DCTCompression dctCompression;
+
+    auto img_vec = img.getImageAsVector();
+
+    auto compressedImage = dctCompression.DCTCompress(img_vec, CompressionLevel::ULTRA_HIGH);
+
+    auto decompressedImage = dctCompression.DCTDecompress(compressedImage, CompressionLevel::ULTRA_HIGH, static_cast<int>(img_vec.size()), static_cast<int>(img_vec[0].size()));
+
+    Image decompressedImg = Image(decompressedImage);
+
+    decompressedImg.displayImage("Decompressed Image");
+
+    decompressedImg.DisplayDiffImage(img);
 
     return 0;
 }
