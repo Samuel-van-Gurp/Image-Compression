18e15e4df3575c06addf33ab5da72ed9e13f31c7
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-18 09:20:30 +0100

    Add CompressionLevel enum and QuantizationTable structure for image compression

---
+++ src/DCT/QuantizationTable.h
@@ -0,0 +1,15 @@
+#ifndef JPEGQUANTIZATIONTABLE_H
+#define JPEGQUANTIZATIONTABLE_H
+
+#include <vector>
+
+struct QuantizationTable
+{
+    // Static member variables for different compression levels
+    static const std::vector<std::vector<int>> lowCompressionTable;
+    static const std::vector<std::vector<int>> mediumCompressionTable;
+    static const std::vector<std::vector<int>> highCompressionTable;
+    static const std::vector<std::vector<int>> veryHighCompressionTable;
+};
+
+#endif // JPEGQUANTIZATIONTABLE_H
