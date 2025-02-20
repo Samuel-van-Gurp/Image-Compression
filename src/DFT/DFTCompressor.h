5d2dec1d82241d7598b9d25fe196aa40af8e1a54
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-20 13:36:54 +0100

    Remove unused DFT methods from DFTCompressor.h

--- src/DFT/DFTCompressor.h
+++ src/DFT/DFTCompressor.h
@@ -13,12 +13,6 @@ public:
     Image decompress(const SparseRepresentation &sparseRepr) const;
 
 private:
-    // cv::Mat computeDFT(const cv::Mat &img) const;
-    // cv::Mat prepareComplex2channelMat(const cv::Mat &img) const;
-    // cv::Mat convertTOFloatMat(const cv::Mat &img) const;
-    // cv::Mat computeInverseDFT(const cv::Mat &complexImage) const;
-    // cv::Mat Magnitude(const cv::Mat &complexImage) const;
-
     float const ComputeIntensityThreshold(const cv::Mat &img, float percentile) const;
     cv::Mat MakeSubSamplingMask(const cv::Mat &img, float threshold) const;
     cv::Mat applyMask(const cv::Mat &complexImage, const cv::Mat &mask) const;
