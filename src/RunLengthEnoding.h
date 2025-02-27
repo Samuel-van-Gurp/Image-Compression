613461a9ff92aff39c43a4f58887629e19bf8ddf
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-27 10:00:09 +0100

    refactor RunLengthEncoding to support templates so all types can be encoded (if they support  <> operator)

--- src/RunLengthEnoding.h
+++ src/RunLengthEnoding.h
@@ -6,15 +6,70 @@
 class RunLengthEnoding
 {
 public:
+    template <typename T>
+    std::vector<std::vector<std::pair<T, int>>> RunLengthEncodeImageBlocks(const std::vector<std::vector<T>> &vector) const
+    {
+        std::vector<std::vector<std::pair<T, int>>> encodedBlocks;
+        for (const auto &block : vector)
+        {
+            auto encodedBlock = RunLengthEncode(block);
+            encodedBlocks.push_back(encodedBlock);
+        }
+        return encodedBlocks;
+    }
 
-    std::vector<std::vector<std::pair<int, int>>> RunLengthEncodeImageBlocks(const std::vector<std::vector<int>> &vector) const;
-    std::vector<std::vector<int>> RunLengthDecodeImageBlocks(const std::vector<std::vector<std::pair<int, int>>> &encodedVector) const;
+    template <typename T>
+    std::vector<std::vector<T>> RunLengthDecodeImageBlocks(const std::vector<std::vector<std::pair<T, int>>> &encodedVector) const
+    {
+        std::vector<std::vector<T>> decodedBlocks;
+        for (const auto &block : encodedVector)
+        {
+            auto decodedBlock = RunLengthDecode(block);
+            decodedBlocks.push_back(decodedBlock);
+        }
+        return decodedBlocks;
+    }
 
+    template <typename T>
+    std::vector<std::pair<T, int>> RunLengthEncode(const std::vector<T> &vector) const
+    {
+        std::vector<std::pair<T, int>> encoded;
+        if (vector.empty())
+            return encoded;
 
-    std::vector<std::pair<int, int>> RunLengthEncode(const std::vector<int> &vector) const;
-    std::vector<int> RunLengthDecode(const std::vector<std::pair<int, int>> &encodedVector) const;
+        T currentValue = vector[0];
+        int count = 1;
 
-private:
+        for (size_t i = 1; i < vector.size(); i++)
+        {
+            if (vector[i] == currentValue)
+            {
+                count++;
+            }
+            else
+            {
+                encoded.push_back({currentValue, count});
+                currentValue = vector[i];
+                count = 1;
+            }
+        }
+        encoded.push_back({currentValue, count});
+        return encoded;
+    }
+
+    template <typename T>
+    std::vector<T> RunLengthDecode(const std::vector<std::pair<T, int>> &encodedVector) const
+    {
+        std::vector<T> decodedVector;
+        for (const auto &element : encodedVector)
+        {
+            for (int i = 0; i < element.second; i++)
+            {
+                decodedVector.push_back(element.first);
+            }
+        }
+        return decodedVector;
+    }
 };
 
 #endif // RUN_LENGTH_ENCODING_H
