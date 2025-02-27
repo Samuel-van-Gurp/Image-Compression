7048642d7dc9751a8d952b628fa6846f567e0a2e
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-27 16:24:47 +0100

    Add public members to CompressedImageHolder for decompression data and quantization table

--- src/DCT/CompressedImageHolder.h
+++ src/DCT/CompressedImageHolder.h
@@ -1,13 +1,18 @@
 #ifndef COMPRESSED_IMAGE_HOLDER_H
 #define COMPRESSED_IMAGE_HOLDER_H
 
+#include "QuantizationTable.h"
 #include <vector>
 
 struct CompressedImageHolder
 {
-    
+public:
+    // All the info needed to decompress the image later on
+    std::vector<std::vector<std::pair<float, int>>> compressedImage;
+    std::pair<int, int> OriginalImageDimensions;
+    int BLOCK_SIZE;
 
+    std::vector<std::vector<int>> quantizationTable;
 };
- 
 
 #endif // COMPRESSED_IMAGE_HOLDER_H
