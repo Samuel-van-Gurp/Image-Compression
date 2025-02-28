b655117c3ff3b9ab13ac1d65a5ce8ecf187acf01
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-28 16:40:01 +0100

    Update DCTCompression to use CompressedDCTImageHolder and CompressionLevel for compression methods

--- src/DCT/DCTCompression.cpp
+++ src/DCT/DCTCompression.cpp
@@ -1,6 +1,6 @@
 #include "DCTCompression.h"
 
-CompressedImageHolder DCTCompression::DCTCompress(const std::vector<std::vector<float>> &image, const std::vector<std::vector<int>> &quantizationTable) const
+CompressedDCTImageHolder DCTCompression::DCTCompress(const std::vector<std::vector<float>> &image, CompressionLevel compressionLevel) const
 {
     DCTTransformationHandler dctTransformationHandler(static_cast<int>(image.size()));
     RunLengthEnoding runLengthEncoding;
@@ -12,12 +12,14 @@ CompressedImageHolder DCTCompression::DCTCompress(const std::vector<std::vector<
         throw std::invalid_argument("Image is empty");
     }
 
+    auto quantizationTable = QuantizationTable::getQuantizationTable(compressionLevel);
+
     auto tranformedImage = dctTransformationHandler.DCTTransformImage(image, quantizationTable);
 
     std::vector<std::vector<std::pair<float, int>>> encodedImage = dctEncoding.encodeImageBlocks(tranformedImage);
 
     // fill the compressed image holder
-    CompressedImageHolder compressedImageHolder;
+    CompressedDCTImageHolder compressedImageHolder;
     compressedImageHolder.quantizationTable = quantizationTable;
     compressedImageHolder.compressedImage = encodedImage;
     compressedImageHolder.OriginalImageDimensions = std::make_pair(static_cast<int>(image.size()), static_cast<int>(image[0].size()));
@@ -26,21 +28,19 @@ CompressedImageHolder DCTCompression::DCTCompress(const std::vector<std::vector<
     return compressedImageHolder;
 }
 
-std::vector<std::vector<float>> DCTCompression::DCTDecompress(CompressedImageHolder &compressedImageHolder) const
+std::vector<std::vector<float>> DCTCompression::DCTDecompress(CompressedDCTImageHolder &compressedImageHolder) const
 {
-    // DCTTransformationHandler dctTransformationHandler = DCTTransformationHandler(CompressedImageHolder.BLOCK_SIZE);
     DCTTransformationHandler dctTransformationHandler(compressedImageHolder.BLOCK_SIZE);
 
     RunLengthEnoding runLengthEncoding;
     ZigzagDCTcoefficientsOrder zigzagOrder;
     DCTEncoding dctEncoding(runLengthEncoding, zigzagOrder);
 
-
     auto decoded = dctEncoding.decodeImageBlocks(compressedImageHolder.compressedImage);
 
-    auto ReconstructedImage = dctTransformationHandler.inverseDCTTransformImage(decoded, compressedImageHolder.quantizationTable, 
-                                                                                         compressedImageHolder.OriginalImageDimensions.first, 
-                                                                                         compressedImageHolder.OriginalImageDimensions.second);
-    
+    auto ReconstructedImage = dctTransformationHandler.inverseDCTTransformImage(decoded, compressedImageHolder.quantizationTable,
+                                                                                compressedImageHolder.OriginalImageDimensions.first,
+                                                                                compressedImageHolder.OriginalImageDimensions.second);
+
     return ReconstructedImage;
 }
--- src/DCT/DCTCompression.h
+++ src/DCT/DCTCompression.h
@@ -1,18 +1,20 @@
 #ifndef DCT_COMPRESSION_H
 #define DCT_COMPRESSION_H
 
+#include "CompressedDCTImageHolder.h"
 #include "compressedImageHolder.h"
 #include "DCTTransformationHandler.h"
 #include "QuantizationTable.h"
 #include "DCTEncoding.h"
+#include "ICompressionStrategy.h" 
 
 #include <algorithm>
 #include <vector>
-class DCTCompression
+class DCTCompression // : public ICompressionStrategy TODO
 {
 public:
-    CompressedImageHolder DCTCompress(const std::vector<std::vector<float>> &image, const std::vector<std::vector<int>> &quantizationTable) const;
-    std::vector<std::vector<float>> DCTDecompress(CompressedImageHolder &compressedImageHolder) const;
+    CompressedDCTImageHolder DCTCompress(const std::vector<std::vector<float>> &image, CompressionLevel compressionLevel) const;
+    std::vector<std::vector<float>> DCTDecompress(CompressedDCTImageHolder &compressedImageHolder) const;
 
 private:
 };
