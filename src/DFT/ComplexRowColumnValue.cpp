cf81f301a5b399b9b3092d0fa3021495394ed16d
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-28 16:41:05 +0100

    renamed ComplexRowColumnValue class and its header file

--- src/DFT/ComplexRowColumnValue.cpp
+++
@@ -1,6 +0,0 @@
-#include "ComplexRowColumnValue.h"
-
-ComplexRowColumnValue::ComplexRowColumnValue(int row, int col, float real, float imag)
-    : m_row(row), m_col(col), m_real(real), m_imag(imag)
-{
-}
