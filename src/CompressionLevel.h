c618b94e2a80f71ebb8aad16d6ad91a09190ae34
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-28 16:38:46 +0100

    Add CompressionLevel enumeration for image compression settings

---
+++ src/CompressionLevel.h
@@ -0,0 +1,15 @@
+#ifndef COMPRESSION_LEVEL_H
+#define COMPRESSION_LEVEL_H
+
+
+enum class CompressionLevel
+{
+    NONE,
+    LOW,    
+    MEDIUM, 
+    HIGH,
+    VERY_HIGH,
+    ULTRA_HIGH 
+};
+
+#endif  // COMPRESSION_LEVEL_H
