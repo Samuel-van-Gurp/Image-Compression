c3743ee9954c418e96d5e8624e27b0f23bfb9a13
PROD\svangurp <samuel.vangurp@alten.nl>
2025-02-28 10:28:22 +0100

    BUG FIX: fixed writing of the runlength encoded image

--- src/DCT/StoreDCTData.cpp
+++ src/DCT/StoreDCTData.cpp
@@ -12,7 +12,7 @@ void StoreDCTData::writeToBinary(const std::string &fileName, const std::string 
     outFile.write(reinterpret_cast<const char *>(&compressedImageHolder.BLOCK_SIZE), sizeof(compressedImageHolder.BLOCK_SIZE));
 
     writeOriginalImageSize(outFile, compressedImageHolder.OriginalImageDimensions);
-    
+
     writeQuantizationTable(outFile, compressedImageHolder.quantizationTable);
 
     writeCompressedImage(outFile, compressedImageHolder.compressedImage);
@@ -44,18 +44,25 @@ CompressedImageHolder StoreDCTData::readFromBinary(const std::string &fileName, 
 
 std::vector<std::vector<std::pair<float, int>>> StoreDCTData::readCompressedImage(std::ifstream &inFile)
 {
-    size_t numRows, numCols;
-    inFile.read(reinterpret_cast<char *>(&numRows), sizeof(numRows));
-    inFile.read(reinterpret_cast<char *>(&numCols), sizeof(numCols));
+    // Read number of blocks
+    int numBlocks;
+    inFile.read(reinterpret_cast<char *>(&numBlocks), sizeof(numBlocks));
 
-    std::vector<std::vector<std::pair<float, int>>> compressedImage(numRows, std::vector<std::pair<float, int>>(numCols));
+    std::vector<std::vector<std::pair<float, int>>> compressedImage(numBlocks);
 
-    for (size_t i = 0; i < numRows; ++i)
+    // For each block
+    for (int i = 0; i < numBlocks; ++i)
     {
-        for (size_t j = 0; j < numCols; ++j)
+        // Read number of pairs in this block
+        int numPairs;
+        inFile.read(reinterpret_cast<char *>(&numPairs), sizeof(numPairs));
+
+        // Read each value-runlength pair
+        compressedImage[i].resize(numPairs);
+        for (int j = 0; j < numPairs; ++j)
         {
-            inFile.read(reinterpret_cast<char *>(&compressedImage[i][j].first), sizeof(compressedImage[i][j].first));
-            inFile.read(reinterpret_cast<char *>(&compressedImage[i][j].second), sizeof(compressedImage[i][j].second));
+            inFile.read(reinterpret_cast<char *>(&compressedImage[i][j].first), sizeof(float));
+            inFile.read(reinterpret_cast<char *>(&compressedImage[i][j].second), sizeof(int));
         }
     }
 
@@ -64,15 +71,15 @@ std::vector<std::vector<std::pair<float, int>>> StoreDCTData::readCompressedImag
 
 std::vector<std::vector<int>> StoreDCTData::readQuantizationTable(std::ifstream &inFile)
 {
-    size_t numRows, numCols;
+    int numRows, numCols;
     inFile.read(reinterpret_cast<char *>(&numRows), sizeof(numRows));
     inFile.read(reinterpret_cast<char *>(&numCols), sizeof(numCols));
 
     std::vector<std::vector<int>> quantizationTable(numRows, std::vector<int>(numCols));
 
-    for (size_t i = 0; i < numRows; ++i)
+    for (int i = 0; i < numRows; ++i)
     {
-        for (size_t j = 0; j < numCols; ++j)
+        for (int j = 0; j < numCols; ++j)
         {
             inFile.read(reinterpret_cast<char *>(&quantizationTable[i][j]), sizeof(quantizationTable[i][j]));
         }
@@ -81,7 +88,7 @@ std::vector<std::vector<int>> StoreDCTData::readQuantizationTable(std::ifstream 
     return quantizationTable;
 }
 
-std::pair<int, int> StoreDCTData::readOriginalImageSize(std::ifstream &inFile) 
+std::pair<int, int> StoreDCTData::readOriginalImageSize(std::ifstream &inFile)
 {
     int rows, cols;
     inFile.read(reinterpret_cast<char *>(&rows), sizeof(rows));
@@ -92,19 +99,25 @@ std::pair<int, int> StoreDCTData::readOriginalImageSize(std::ifstream &inFile)
 
 void StoreDCTData::writeCompressedImage(std::ofstream &outFile, const std::vector<std::vector<std::pair<float, int>>> &compressedImage)
 {
-    size_t numRows = compressedImage.size();
-    size_t numCols = compressedImage[0].size();
-
-    outFile.write(reinterpret_cast<const char *>(&numRows), sizeof(numRows));
-    outFile.write(reinterpret_cast<const char *>(&numCols), sizeof(numCols));
-
-    for (size_t i = 0; i < numRows; ++i)
+    if (compressedImage.empty())
     {
-        for (size_t j = 0; j < numCols; ++j)
-        {
-            float value = compressedImage[i][j].first;
-            int runLength = compressedImage[i][j].second;
+        throw std::runtime_error("Compressed image data is empty");
+    }
 
+    // Write number of blocks
+    int numBlocks = static_cast<int>(compressedImage.size());
+    outFile.write(reinterpret_cast<const char *>(&numBlocks), sizeof(numBlocks));
+
+    // For each block
+    for (const auto &block : compressedImage)
+    {
+        // Write number of pairs in this block
+        int numPairs = static_cast<int>(block.size());
+        outFile.write(reinterpret_cast<const char *>(&numPairs), sizeof(numPairs));
+
+        // Write each value-runlength pair
+        for (const auto &[value, runLength] : block)
+        {
             outFile.write(reinterpret_cast<const char *>(&value), sizeof(value));
             outFile.write(reinterpret_cast<const char *>(&runLength), sizeof(runLength));
         }
@@ -113,23 +126,21 @@ void StoreDCTData::writeCompressedImage(std::ofstream &outFile, const std::vecto
 
 void StoreDCTData::writeQuantizationTable(std::ofstream &outFile, const std::vector<std::vector<int>> &quantizationTable)
 {
-    size_t numRows = quantizationTable.size();
-    size_t numCols = quantizationTable[0].size();
+    int numRows = static_cast<int>(quantizationTable.size());
+    int numCols = static_cast<int>(quantizationTable[0].size());
 
     outFile.write(reinterpret_cast<const char *>(&numRows), sizeof(numRows));
     outFile.write(reinterpret_cast<const char *>(&numCols), sizeof(numCols));
 
-    for (size_t i = 0; i < numRows; ++i)
+    for (int i = 0; i < numRows; ++i)
     {
-        for (size_t j = 0; j < numCols; ++j)
+        for (int j = 0; j < numCols; ++j)
         {
             int value = quantizationTable[i][j];
             outFile.write(reinterpret_cast<const char *>(&value), sizeof(value));
         }
     }
 }
-
-
 
 void StoreDCTData::writeOriginalImageSize(std::ofstream &outFile, const std::pair<int, int> &imageSize) const
 {
