9185f650baf19ff3ff0fd449c3ea2830a4f09ccd
PROD\svangurp <samuel.vangurp@alten.nl>
2025-03-27 11:20:17 +0100

    make singel block encoder public

--- src/DCT/ZigzagDCTcoefficientsOrder.h
+++ src/DCT/ZigzagDCTcoefficientsOrder.h
@@ -1,26 +1,23 @@
 #ifndef ZIGZAG_DCT_COEFFICIENTS_ORDER_H
 #define ZIGZAG_DCT_COEFFICIENTS_ORDER_H
 
-#include "RunLengthEnoding.h"
 #include <vector>
 #include <cmath>
 
 class ZigzagDCTcoefficientsOrder
 {
 public:
-    std::vector<std::vector<float>> ZigZagOrderImageBlocks( std::vector<std::vector<std::vector<float>>> &imageChuncks) const;
+    std::vector<std::vector<float>> ZigZagOrderImageBlocks(std::vector<std::vector<std::vector<float>>> &imageChuncks) const;
 
     std::vector<std::vector<std::vector<float>>> deZigZagOrderImageBlocks(const std::vector<std::vector<float>> &imageChuncks) const;
-    
-private:
-    RunLengthEnoding m_runlengthEnoding;
 
+    std::vector<float> ZigzagOrder(const std::vector<std::vector<float>> &imageBlock) const;
+    std::vector<std::vector<float>> ReverseZigzagtraversal(const std::vector<float> &zigzagOrder) const;
+private:
     void fillZigzagDiagonal(std::vector<std::vector<float>> &imageBlock, const std::vector<float> &zigzagOrder, int sum, int dctBlockSize, int &index) const;
     std::vector<float> Zigzagtraversal(const std::vector<std::vector<float>> &imageBlock);
-    std::vector<float> ZigzagOrder(const std::vector<std::vector<float>> &imageBlock) const;
     std::vector<float> extractZigzagDiagonal(const std::vector<std::vector<float>> &imageBlock, int sum) const;
 
-    std::vector<std::vector<float>> ReverseZigzagtraversal(const std::vector<float> &zigzagOrder) const;
 };
 
 #endif // ZIGZAG_DCT_COEFFICIENTS_ORDER_H
